bce15726d3a12b4be6e4f191fb1883059052b777
R Greene <greenbert81@gmail.com>
2022-08-03 15:51:12 -0700

    updated db credentials for production

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 
 const mysql = require('mysql2');
 
+let connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    }).promise();
+
+}else {
+    connection = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'todos_db',
 }).promise();
+}
 
 module.exports = connection;
