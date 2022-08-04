48ad5ac0451f01e3bae5acd69d88a634ca669e0a
R Greene <greenbert81@gmail.com>
2022-08-03 20:01:33 -0700

    updated todoRoutes index.js

--- routes/apiRoutes/todoRoutes/index.js
+++ routes/apiRoutes/todoRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const connection = require('./../../../db/connection');
+const {getTodoById} = require("../../../model/queries");
 
 const isEven = function(number) {
     return new Promise((resolve, reject) => {
@@ -15,8 +16,8 @@ const isEven = function(number) {
 router.get('/', async (req, res) => {
     // run codes in try block, if it fails skip to catch block and run the code
     try {
-        const getAllTodos = 'SELECT * FROM todos'
-        const [ todos ] = await connection.query(getAllTodos);
+        const getAllTodos = 'SELECT * FROM todos;';
+        const [todos] = await connection.query(getAllTodos);
         res.json(todos);
 
     } catch (e) {
@@ -26,24 +27,80 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-   const { todo } = req.body;
+   const {todo} = req.body;
 
    if (todo.trim().length === 0) {
     return res.status(400).json({ error: 'Todo must be valid'});
 
    }
    const insertTodoQuery = 'INSERT INTO todos (todo) VALUES(?);';
-   const getTodoById = 'SELECT * FROM todos WHERE id = ? LIMIT 1;';
+   const getTodoById = 'SELECT * FROM todos WHERE id = ?;';
 
    try {
-    const [ queryResult ] = await connection.query(insertTodoQuery, [todo]);
-    const [ todos ] = await connection.query(getTodoById, [queryResult.insertId]);
+    const [queryResult] = await connection.query(insertTodoQuery, [todo]);
+    /* [ { howManyRowsWereInserted, insertId,  }, null]  */
+    const [todos] = await connection.query(getTodoById, [queryResult.insertId]);
     res.json(todos[0]);
-   }
-   catch(error) {
+   } catch(error) {
     res.status(500).json(error);
 
    }
 });
+
+/*
+*
+fetch(`/api/todos/${todoId}`, {
+  method: 'DELETE',
+}).then(res => res.json()
+.then(deletedTodo => console.log(deletedTodo));
+* */
+
+router.delete('/:todoId', async (req, res) => {
+    const {todoId} = req.params;
+  
+    const getTodoById = 'SELECT * FROM todos WHERE id = ?;';
+    const deleteTodoById = 'DELETE FROM todos WHERE id = ?;';
+    try {
+      const [todos] = await connection.query(getTodoById, todoId);
+      if (todos.length === 0) {
+        return res.status(404).json({error: 'Todo not found with that id'});
+      }
+      await connection.query(deleteTodoById, todoId);
+      res.json(todos[0]);
+    } catch (error) {
+      res.status(500).json({error});
+    }
+  });
+
+
+  /*
+*
+fetch(`/api/todos/${todoId}`, {
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ todo: 'Run 3 miles'})
+}).then(res => res.json()
+.then(deletedTodo => console.log(deletedTodo));
+* */
+
+
+router.patch('/:todoId', async (req, res) => {
+    const {todo} = req.body;
+    const {todoId} = req.params;
+    if (todo.trim().length === 0) {
+      return res.status(400).json({error: 'Todo must be provided'});
+    }
+  
+    const updateTodoById = 'UPDATE todos SET todo = ? WHERE id = ?;';
+    try {
+      await connection.query(updateTodoById, [todo, todoId]);
+      const [todos] = await connection.query(getTodoById, todoId);
+      res.json(todos[0]);
+    } catch (error) {
+      res.status(500).json({error});
+    }
+  });
 
 module.exports = router;
