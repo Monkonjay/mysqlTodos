5195d3e1202982d226ae1deb9ada525e6a443700
R Greene <greenbert81@gmail.com>
2022-08-03 20:09:02 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,35 +1,12 @@
 const express = require('express');
-const mysql = require('mysql2');
-// const routes = require('./routes');
-
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'todos_db',
-//     password: 'password',
-// })
+const routes = require('./routes');
 
 const app = express();
-
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-// app.use(routes);
 
-// app.get('/api/todos', (req, res) => {
-//     connection.query('SELECT * FROM todos;' , (err, rows) => {
-//         if (err) {
-//             return res.status(404).json(err);
-//         }
-
-//         console.log("**************** I AM ROWS**************");
-//         console.log(rows);
-//         res.json(rows);
-       
-
-//     });
-// });
-
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
